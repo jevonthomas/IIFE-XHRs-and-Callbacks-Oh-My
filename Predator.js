ca365ebf3864ab9bee60db25c43a960dfa5bec40
jevonthomas <jevon.g.thomas@gmail.com>
2017-07-10 08:54:04 -0500

    Predator exposes two public functions to load each JSON file and store the array of animals in the appropriate private array. Each of those functions accept one argument that will store the callback function to be executed

--- Predator.js
+++ Predator.js
@@ -1,4 +1,4 @@
-var Predator = (function () {
+let Predator = (function () {
   // Predator should have two private arrays to store carnivores and herbivores.
   let carnivores = [];
   let herbivores = [];
@@ -9,13 +9,32 @@ var Predator = (function () {
 
       loader.addEventListener("load", function () {
         // Set the value of the private array
-        carnivores = JSON.parse(this.responseText);
-
+        carnivores = JSON.parse(this.responseText).carnivore;
         // Invoke the callback function so that the caller knows
         // that the process is complete. Make sure to pass the
         // carnivore array as an argument.
-
+        callbackToInvoke(carnivores);
       });
+      //Tell which HTTP verb to use: GET, POST, PUT, DELETE, PATCH
+      loader.open("GET", "carnivore.json");
+      //Get request
+      loader.send();
+    },
+    loadHerbivores: function (callbackFunction) {
+      let loader = new XMLHttpRequest();
+
+      loader.addEventListener("load", function () {
+        // Set the value of the private array
+        herbivores = JSON.parse(this.responseText).herbivore;
+        // Invoke the callback function so that the caller knows
+        // that the process is complete. Make sure to pass the
+        // carnivore array as an argument.
+        callbackFunction(herbivores);
+      });
+      //Tell which HTTP verb to use: GET, POST, PUT, DELETE, PATCH
+      loader.open("GET", "herbivore.json");
+      //Get request
+      loader.send();
     }
-  }
+  };
 })();
--- iife-xhr.js
+++ iife-xhr.js
@@ -1,9 +1,16 @@
 function showCarnivores (carnivores) {
-
+	let domPosition = document.getElementById("carn-container");
+	for (let i = 0; i < carnivores.length; i++) {
+		domPosition.innerHTML += `<h1>${carnivores[i]}</h1>`;
+	}
 }
 
-function showHerbivores () {
-
+function showHerbivores (herbivores) {
+	let domPosition = document.getElementById("herb-container");
+	for (let i = 0; i < herbivores.length; i++) {
+		domPosition.innerHTML += `<h1>${herbivores[i]}</h1>`;
+	}
 }
 
 Predator.loadCarnivores(showCarnivores);
+Predator.loadHerbivores(showHerbivores);
